6d8190c7044132d346bb4463344c6292abdb208c
Aid3n_Pearce <61540835+Aid-3n@users.noreply.github.com>
2020-06-17 22:01:49 +0530

    Update gmute.py

--- userbot/plugins/gmute.py
+++ userbot/plugins/gmute.py
@@ -3,7 +3,7 @@ import asyncio
 from userbot.utils import admin_cmd
 
 #@command(outgoing=True, pattern=r"^.gmute ?(\d+)?")
-@borg.on(admin_cmd(pattern=r"gmute ?(\d+)?"))
+@borg.on(admin_cmd(pattern=r".gmute ?(\d+)?"))
 async def startgmute(event):
     private = False
     if event.fwd_from:
@@ -33,7 +33,7 @@ async def startgmute(event):
         await event.edit("Successfully gmuted that person")
 
 #@command(outgoing=True, pattern=r"^.ungmute ?(\d+)?")
-@borg.on(admin_cmd(pattern=r"ungmute ?(\d+)?"))
+@borg.on(admin_cmd(pattern=r".ungmute ?(\d+)?"))
 async def endgmute(event):
     private = False
     if event.fwd_from:
